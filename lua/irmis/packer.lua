f2210b127a2926725dcfbdc8a88b908b0d9ea0ee
Irmantas Siupsinskas <i.siupsinskas@gmail.com>
2023-01-08 12:56:00 +0200

    feat: add lualine and ts context

--- lua/irmis/packer.lua
+++ lua/irmis/packer.lua
@@ -1,38 +1,45 @@
 return require("packer").startup(function(use)
-	use("wbthomason/packer.nvim")
+    use("wbthomason/packer.nvim")
 
-	use("luisiacc/gruvbox-baby")
+    use("luisiacc/gruvbox-baby")
 
-	use({
-		"nvim-telescope/telescope.nvim",
-		branch = "0.1.x",
-		requires = {
-			{ "nvim-lua/plenary.nvim" }
-		}
-	})
+    use({
+        "nvim-telescope/telescope.nvim",
+        branch = "0.1.x",
+        requires = {
+            { "nvim-lua/plenary.nvim" }
+        }
+    })
 
-	use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
-	use {
-		'VonHeikemen/lsp-zero.nvim',
-		requires = {
-			-- LSP Support
-			{ 'neovim/nvim-lspconfig' },
-			{ 'williamboman/mason.nvim' },
-			{ 'williamboman/mason-lspconfig.nvim' },
+    use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
+    use("nvim-treesitter/nvim-treesitter-context")
 
-			-- Autocompletion
-			{ 'hrsh7th/nvim-cmp' },
-			{ 'hrsh7th/cmp-buffer' },
-			{ 'hrsh7th/cmp-path' },
-			{ 'saadparwaiz1/cmp_luasnip' },
-			{ 'hrsh7th/cmp-nvim-lsp' },
-			{ 'hrsh7th/cmp-nvim-lua' },
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        requires = {
+            -- LSP Support
+            { 'neovim/nvim-lspconfig' },
+            { 'williamboman/mason.nvim' },
+            { 'williamboman/mason-lspconfig.nvim' },
 
-			-- Snippets
-			{ 'L3MON4D3/LuaSnip' },
-			{ 'rafamadriz/friendly-snippets' },
-		}
-	}
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' },
+            { 'hrsh7th/cmp-buffer' },
+            { 'hrsh7th/cmp-path' },
+            { 'saadparwaiz1/cmp_luasnip' },
+            { 'hrsh7th/cmp-nvim-lsp' },
+            { 'hrsh7th/cmp-nvim-lua' },
 
-	use("lukas-reineke/indent-blankline.nvim")
+            -- Snippets
+            { 'L3MON4D3/LuaSnip' },
+            { 'rafamadriz/friendly-snippets' },
+        }
+    }
+
+    use("lukas-reineke/indent-blankline.nvim")
+
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+    }
 end)
